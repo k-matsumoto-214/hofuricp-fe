a9a12c0451674b198ac67496fe58aa30929df231
k-matsumoto-214 <keisuke.matsumoto214+github@gmail.com>
2023-09-26 06:53:18 +0900

    重複行を削除

--- src/components/CpDailyAmountTable.tsx
+++ src/components/CpDailyAmountTable.tsx
@@ -41,8 +41,6 @@ export const CpDailyAmountTable = (props: Props): JSX.Element => {
 
   dates = (data as CpDailyAmountResponse).dates;
   cpDailyAmountDatas = (data as CpDailyAmountResponse).cpDailyAmountDatas;
-  dates = (data as CpDailyAmountResponse).dates;
-  cpDailyAmountDatas = (data as CpDailyAmountResponse).cpDailyAmountDatas;
 
   if (dates.length === 0) {
     return (
@@ -58,7 +56,6 @@ export const CpDailyAmountTable = (props: Props): JSX.Element => {
     <tr>
       <th></th>
       {dates.map((date) => {
-        return <th key={format(date)}>{format(date)}</th>;
         return <th key={format(date)}>{format(date)}</th>;
       })}
     </tr>
@@ -125,11 +122,6 @@ export const CpDailyAmountTable = (props: Props): JSX.Element => {
 
   return (
     <main className={styles.main}>
-      <div>
-        <button className={styles.excelButton} onClick={(e) => dlExcel(e)}>
-          エクセル DL
-        </button>
-      </div>
       <div>
         <button className={styles.excelButton} onClick={(e) => dlExcel(e)}>
           エクセル DL
